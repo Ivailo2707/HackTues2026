58177da12be9c8e4bcc40580f87c17cd32075b99
Vasil Velev <vasilvelev07@gmail.com>
2026-03-26 13:57:50 +0200

    spring boot skeleton added

---
+++ backend/src/test/java/com/cm/ecohub/EcohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.ecohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ee6c3c1cc2d064b73b65c58584af39838d2a247a
Vasil Velev <vasilvelev07@gmail.com>
2026-03-27 11:13:51 +0200

    backend integration with axios and the api for 'user' completed

---
+++ frontend/src/lib/error.js
@@ -0,0 +1,34 @@
+export const normalizeError = (error) => {
+    if (!error.response) {
+        return {
+            message: "Cannot connect to server.",
+            errors: {},
+        };
+    }
+
+    const { status, data } = error.response;
+
+    let result = {
+        message: "An unexpected error occurred.",
+        errors: {},
+    };
+
+    if (status === 400) {
+        if (data.error) {
+            result.message = data.error;
+        } else {
+            result.message = "Please correct the errors below.";
+            result.errors = data;
+        }
+    } else if (status === 401) {
+        result.message = "Invalid email or password.";
+    } else if (status === 403) {
+        result.message = "Access denied (Account disabled or forbidden).";
+    } else if (status === 404) {
+        result.message = "Resource not found.";
+    } else if (status >= 500) {
+        result.message = "Server error. Please try again later.";
+    }
+
+    return result;
+};
---
+++ frontend/src/lib/http.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { normalizeError } from "./error";
+import { API_PATHS } from "../constants";
+
+const http = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json"
+    },
+});
+
+http.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("accessToken");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(normalizeError(error))
+);
+
+http.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        const status = error.response?.status;
+
+        if (originalRequest.url.includes(API_PATHS.AUTH_REFRESH)) {
+            localStorage.removeItem("accessToken");
+            return Promise.reject(normalizeError(error));
+        }
+
+        if (status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const response = await http.post(API_PATHS.AUTH_REFRESH);
+                const newAccessToken = await response.data.token;
+
+                localStorage.setItem("accessToken", newAccessToken);
+                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+                return http(originalRequest);
+            } catch (refreshError) {
+                localStorage.removeItem("accessToken");
+                return Promise.reject(normalizeError(error));
+            }
+        }
+
+        return Promise.reject(normalizeError(error));
+    }
+);
+
+export default http;
